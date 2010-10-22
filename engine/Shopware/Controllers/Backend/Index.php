d7246f91302b4b0443101db10e939665edde856b
Stefan Hamann <sth@shopware.de>
2010-10-22 14:16:04 +0000

    Fixing

--- engine/Shopware/Controllers/Backend/Index.php
+++ engine/Shopware/Controllers/Backend/Index.php
@@ -3,8 +3,6 @@ class Shopware_Controllers_Backend_Index extends Enlight_Controller_Action
 {	
 	public function indexAction()
 	{
-		//$menu = $this->_getMenu();
-		
 		Shopware()->Plugins()->Core()->License()->checkLicense('sPREMIUM');
 		
 		$this->View()->Menu = Shopware()->Menu();
@@ -35,6 +33,8 @@ class Shopware_Controllers_Backend_Index extends Enlight_Controller_Action
 		$this->View()->UserName = $_SESSION['sName'];
 		$connectString = "?domain=".Shopware()->Config()->Host."&pairing=".Shopware()->Config()->AccountId;
 		$this->View()->accountUrl = "https://support.shopware2.de/account2/index.php$connectString";
+		
+		Shopware()->Db()->query("UPDATE s_core_config SET value = '3.5.1' WHERE name='sVERSION'");
 	}
 	
 	public function logoutAction()
