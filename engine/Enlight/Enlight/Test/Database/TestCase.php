7ca76618d988291fc06f86fb4ce6b7fdccb3f7b7
Heiner Lohaus <hl@shopware.de>
2011-02-14 12:57:57 +0000

    "- test suite changes #4857"

--- engine/Enlight/Enlight/Test/Database/TestCase.php
+++ engine/Enlight/Enlight/Test/Database/TestCase.php
@@ -1,10 +1,14 @@
 <?php
 /**
- * Enter description here...
- *
+ * Enlight database test case
  */
 abstract class Enlight_Test_Database_TestCase extends PHPUnit_Extensions_Database_TestCase
 {
+	/**
+     * Returns the test database connection.
+     *
+     * @return PHPUnit_Extensions_Database_DB_IDatabaseConnection
+     */
 	protected function getConnection()
 	{
 		$pdo = Enlight::Instance()->Db()->getConnection();
