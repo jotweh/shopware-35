067cbbb1d07c7c85f6a9e44f1c44294321fd6294
Stefan Hamann <sth@shopware.de>
2011-04-20 09:11:12 +0000

    Commit Ticket #5635 Adding Selenium Basics

--- engine/Enlight/Enlight/Components/Test/Selenium/TestCase.php
+++ engine/Enlight/Enlight/Components/Test/Selenium/TestCase.php
@@ -10,5 +10,16 @@
  */
 abstract class Enlight_Components_Test_Selenium_TestCase extends PHPUnit_Extensions_SeleniumTestCase
 {
+    protected $captureScreenshotOnFailure = TRUE;
+    protected $screenshotPath = '/var/www/';
+    protected $screenshotUrl = 'http://localhost/';
 
+	/**
+	 * Setup Shop - Set base url
+	 * @return void
+	 */
+    protected function setUp()
+    {
+        $this->setBrowserUrl('http://daily.shopvm.de/');
+    }
 }
