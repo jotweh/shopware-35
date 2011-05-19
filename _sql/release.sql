9147c9f634e7fe9eb8fa2980e9ee647bcd84d7f6
Heiner Lohaus <hl@shopware.de>
2011-05-19 10:54:53 +0000

    - add rfi input filter config #4832

--- _sql/release.sql
+++ _sql/release.sql
@@ -90,3 +90,9 @@ UPDATE `s_core_config_mails` SET `name` = TRIM(`name`);
 DELETE FROM `s_core_subscribes` WHERE `listener` LIKE 'Shopware_Plugins_Frontend_InputFilter_Bootstrap::%';
 INSERT INTO `s_core_subscribes` (`id`, `subscribe`, `type`, `listener`, `pluginID`, `position`) VALUES
 (NULL, 'Enlight_Controller_Front_RouteStartup', 0, 'Shopware_Plugins_Frontend_InputFilter_Bootstrap::onRouteStartup', 35, -100);
+INSERT IGNORE INTO `s_core_plugin_configs` (`id`, `name`, `value`, `pluginID`, `localeID`, `shopID`) VALUES
+(NULL, 'rfi_protection', 's:1:"1";', 35, 1, 1),
+(NULL, 'rfi_regex', 's:33:"\\.\\./|\\0|2\\.2250738585072011e-308";', 35, 1, 1);
+INSERT IGNORE INTO `s_core_plugin_elements` (`id`, `pluginID`, `name`, `value`, `label`, `description`, `type`, `required`, `order`, `scope`, `filters`, `validators`) VALUES
+(NULL, 35, 'rfi_protection', 'i:1;', 'RemoteFileInclusion-Schutz aktivieren', '', 'Text', 0, 0, 0, NULL, NULL),
+(NULL, 35, 'rfi_regex', 's:33:"\\.\\./|\\0|2\\.2250738585072011e-308";', 'RemoteFileInclusion-Filter', '', 'Text', 0, 0, 0, NULL, NULL);
