4e72be0774de8ab78a3cb197ef738fb9fcd02f82
Stephan Pohl <sp@shopware.de>
2011-07-27 12:00:26 +0000

    Intern #5716 Extern #100485 Update snippet "sMailConfirmation" at localeID 1 (german) due to a spelling error

--- _sql/release.sql
+++ _sql/release.sql
@@ -3,3 +3,12 @@
  * @author st.hamann
  * @since 3.5.4 - 2011/06/06
  */
+
+
+/**
+ * @ticket #5716 (intern)
+ * @ticket #100485 (extern)
+ * @author s.pohl
+ * @date 2011-07-27
+ */
+UPDATE `s_core_snippets` SET `value` = 'Vielen Dank. Wir haben Ihnen eine Bestätigungsemail gesendet. Klicken Sie auf den enthaltenen Link um Ihre Anmeldung zu bestätigen.' WHERE `s_core_snippets`.`localeID` = 1 AND `s_core_snippets`.`name` LIKE 'sMailConfirmation';
